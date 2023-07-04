291adb9e96664b96597b7e213b2e3248d13fe978
Akram Abeslem Chaima <akram@riseup.net>
2023-07-04 22:38:32 +0100

    fix typo

--- main.cpp
+++ main.cpp
@@ -4,10 +4,8 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     FileHandler mainapp(&a);
-
-
     QObject::connect(&mainapp, SIGNAL(finished()), &a, SLOT(quit()), Qt::QueuedConnection);
-mainapp.run(a.arguments());
+    mainapp.run(a.arguments());
 
     return a.exec();
 }
